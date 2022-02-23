f4634db5875913108c228bff43819e17a0c39618
Josué Lobo <darksiders20005@gmail.com>
2022-02-22 22:40:55 -0300

    feat: adiciona validações de user

---
+++ src/controllers/validateUsers.ts
@@ -0,0 +1,67 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validUserName = (req: Request, res: Response, next: NextFunction) => {
+  const { username } = req.body;
+
+  if (!username) return res.status(400)
+    .json({ error: 'Username is required' });
+
+  if (typeof username !== 'string') return res.status(422)
+    .json({ error: 'Username must be a string' });
+
+  if (username.length < 3) return res.status(422)
+    .json({ error: 'Username must be longer than 2 characters' });
+  console.log('entrou user name')
+
+  next();
+};
+
+const validClass = (req: Request, res: Response, next: NextFunction) => {
+  const { classe } = req.body;
+
+  if (!classe) return res.status(400)
+    .json({ error: 'Classe is required' });
+
+  if (typeof classe !== 'string') return res.status(422)
+    .json({ error: 'Classe must be a string' });
+
+  if (classe.length < 3) return res.status(422)
+    .json({ error: 'Classe must be longer than 2 characters' });
+
+  console.log('entrou class')
+  next();
+};
+
+const validLevel = (req: Request, res: Response, next: NextFunction) => {
+  const { level } = req.body;
+
+  if (level < 1) return res.status(422).json({ error: 'Level must be greater than 0' });
+
+  if (!level) return res.status(400)
+    .json({ error: 'Level is required' });
+
+  if (typeof (level) !== 'number') return res.status(422)
+    .json({ error: 'Level must be a number' });
+
+  console.log('entrou level')
+  next();
+};
+
+const validPassword = (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+
+  if (!password) return res.status(400).json({ error: 'Password is required' });
+
+  if (typeof password !== 'string') return res.status(422)
+    .json({ error: 'Password must be a string' });
+
+  if (password.length < 8) return res.status(422)
+    .json({ error: 'Password must be longer than 7 characters' });
+  console.log('entrou password');
+
+  next();
+};
+
+const validate = [validUserName, validClass, validLevel, validPassword];
+
+export default validate;
