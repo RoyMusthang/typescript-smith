7c1d9759f410e235c0f5c4449ba43fc42a6051e0
Josué Lobo <darksiders20005@gmail.com>
2022-02-23 01:48:22 -0300

    refactor: mudando local de validação

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,17 +3,15 @@ import { User } from '../interfaces/User';
 import StatusCode from '../enums/StatusCode';
 import rescue from 'express-rescue';
 import service from '../services/users'
-import validateUser from './validateUsers';
+import validateUser from './validations/validateUsers';
 
 const router = Router();
 
 router.post('/',
   validateUser,
   rescue(async (req: Request, res: Response) => {
-    console.log('entrou')
     const user: User = req.body;
     const newUser: string = await service(user);
-    console.log(StatusCode.CREATED);
 
     res.status(StatusCode.CREATED).json({ token: newUser })
   })
