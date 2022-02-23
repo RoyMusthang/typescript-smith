bb33b84ef8a4c48c6fa9b29d4b6ced1d4059e879
Josué Lobo <darksiders20005@gmail.com>
2022-02-22 22:43:15 -0300

    feat: implementada camada service de create user

---
+++ src/services/users.ts
@@ -0,0 +1,11 @@
+import { User } from "../interfaces/User";
+import { sign } from '../utils/jwt';
+import usersModel from '../models/usersModel';
+
+async function create(user: User): Promise<string> {
+  const newUser = await usersModel.create(user);
+  const token = sign(newUser);
+  return token;
+}
+
+export default create;
