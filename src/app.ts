674d8f1ea7ca4a4a1e30cdf38439d7e3a14d72d9
Josué Lobo <darksiders20005@gmail.com>
2022-02-22 22:34:52 -0300

    feat: modify app to work with folders

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import users from './controllers/users';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/users', users)
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import app from './app';
+import dotenv from 'dotenv';
 
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
