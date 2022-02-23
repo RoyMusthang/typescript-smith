cfbd7f1ce39040e83730617361bc6edb39040d73
Josué Lobo <darksiders20005@gmail.com>
2022-02-23 01:49:20 -0300

    feat: controler de login implemetnado

---
+++ src/controllers/login.ts
@@ -0,0 +1,20 @@
+import { Router, Request, Response } from 'express';
+import StatusCode from '../enums/StatusCode';
+import rescue from 'express-rescue';
+import service from '../services/login';
+import Login from '../interfaces/Login';
+import validate from './validations/validateLogin';
+
+const router = Router();
+
+router.post('/',
+  validate,
+  rescue(async (req: Request, res: Response) => {
+    const user: Login = req.body;
+    const newUser: string = await service(user);
+
+    res.status(StatusCode.OK).json({ token: newUser })
+  })
+);
+
+export default router;
