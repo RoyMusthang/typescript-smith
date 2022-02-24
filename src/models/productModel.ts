4dedbf93cebb58a25ea61575e90dc15dd8b994a6
Josué Lobo <darksiders20005@gmail.com>
2022-02-23 23:02:47 -0300

    feat: implementada camada model de product

---
+++ src/models/productModel.ts
@@ -0,0 +1,16 @@
+import { ResultSetHeader } from 'mysql2';
+import Product from '../interfaces/Product';
+import connection from './connection';
+
+export const insertProduct = async (product: Product): Promise<Product> => {
+  const { name, amount } = product;
+  const query = `INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);`;
+
+  const [result] = await connection.execute<ResultSetHeader>(query, [name, amount]);
+  const { insertId: id } = result;
+  return {
+    id,
+    name,
+    amount,
+  };
+}
