e9ad01a213ffaefbd5c4063f4b4c10de0826386f
Josué Lobo <darksiders20005@gmail.com>
2022-02-23 23:01:04 -0300

    feat: adionando rota de produto e erro

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import users from './controllers/users';
 import login from './controllers/login';
+import product from './controllers/product';
+import domainError from './controllers/domain-error';
 
 dotenv.config();
 
@@ -11,5 +13,7 @@ app.use(express.json());
 
 app.use('/users', users);
 app.use('/login', login);
+app.use('/products', product);
 
+app.use('/', domainError)
 export default app;
