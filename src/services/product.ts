eedc94f753df6bb8419561b822960fec91906b92
Josué Lobo <darksiders20005@gmail.com>
2022-02-23 23:24:43 -0300

    feat: camada service de findAll implementada

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,7 +1,12 @@
 import Product from "../interfaces/Product";
-import { insertProduct } from "../models/productModel";
+import { insertProduct, findAllProduct } from "../models/productModel";
 
 export async function create(product: Product) {
   const newProduct = await insertProduct(product);
   return newProduct as Product
+}
+
+export async function findAll() {
+  const result = await findAllProduct();
+  return result;
 }
